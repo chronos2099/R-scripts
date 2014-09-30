a031368d719b69d2510800382eeac1a223a9c98f
Jack <chrono_dragon@hotmail.com>
2014-09-30 02:03:52 -0400

    semi finished
    needs to match previous map
    needs comments

--- 2.Bayesian Updating in R.r
+++ 2.Bayesian Updating in R.r
@@ -5,6 +5,8 @@ library(dplyr)
 wd <- 'D:/Dropbox/Cardille lab/Work/ascii maps/Bayesian maps'
 setwd(wd)
 
+#bayes<- function(x) {
+#	x*
 
 
 rowsstart <- 3000
@@ -15,7 +17,7 @@ colstart <- 3000
 colend <- 4000
 cols <- colend - colstart
 
-initial<- tbl_df(data.table( Class1 = rep(1/3, rows*cols),Class2 = rep(1/3, rows*cols),Class3 = rep(1/3, rows*cols)))
+
 
 map1<-na.omit(tbl_df(fread(dir()[1]))[rowsstart:rowsend,colstart:colend])
 
@@ -23,6 +25,7 @@ map2<-na.omit(tbl_df(fread((dir()[2])))[rowsstart:rowsend,colstart:colend])
 
 system.time(list1<- tbl_df(as.data.frame(unlist(map1))))#2 minutes
 
+initial<- tbl_df(data.table( Class1 = rep(1/3, nrow(list1)),Class2 = rep(1/3, nrow(list1)),Class3 = rep(1/3, nrow(list1))))
 
 list2<- tbl_df(as.data.frame(unlist(map2)))
 
@@ -33,22 +36,34 @@ names(list2)[1]<- "Classes"
 landscape.class<- na.omit(unique(list1))
 
 
-		
-truthtable1<- list1 %.%
-	mutate (Class1 = ((Classes%/%-9999)==1)*1, Class2 = ((Classes%/%2)==1)*1, Class3 = ((Classes%/%3)==1)*1) %.%
-	select (Class1,Class2,Class3)
-	
-truthtable2<- list2 %.%
-	mutate (Class1 = ((Classes%/%-9999)==1)*1, Class2 = ((Classes%/%2)==1)*1, Class3 = ((Classes%/%3)==1)*1) %.%
-	select (Class1,Class2,Class3)
 
-summarytable<- summarise (truthtable1,Class1sum = sum(Class1), Class2sum= sum(Class2), Class3sum = sum(Class3))
-	
-	
-	
+confusiontable<- table(list2[[1]],list1[[1]])
+
+
+probabilitytable<- apply(confusiontable,2,function(x) x/sum(x))
+
+
+prob.mat<- matrix(0,3,3)	
+colnames(prob.mat)<- c(1,2,3)
+rownames(prob.mat)<- c(1,2,3)
+
+prob.mat[,match(colnames(probabilitytable),colnames(prob.mat))]<- probabilitytable[,na.omit(match(colnames(prob.mat),colnames(probabilitytable)))]
+
+
+prob.mat<- prob.mat[match(list1[[1]],rownames(prob.mat)),]
+
+rownames(prob.mat)<-rownames(list1)
+
+prob.mat<-tbl_df(as.data.frame(prob.mat))
+
+new.mat<- prob.mat*initial
+
+post.prior<-t(apply(new.mat,1,function(x) x/sum(x)))
+
+
+
+
 #probability<- mutate((truthtable*0.925)+0.025)
-
-
 #test <- truthtable[1:100,]
 
 
