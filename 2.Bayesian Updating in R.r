4f6757fdc52d8f134e53c1dd22c969e0208cd15b
Jack <chrono_dragon@hotmail.com>
2014-10-03 10:13:23 -0400

    Needs remake ascii map

--- 2.Bayesian Updating in R.r
+++ 2.Bayesian Updating in R.r
@@ -1,4 +1,3 @@
-library(e1071)
 library(data.table)
 library(dplyr)
 
@@ -23,19 +22,19 @@ map1<-na.omit(tbl_df(fread(dir()[1]))[rowsstart:rowsend,colstart:colend])
 
 map2<-na.omit(tbl_df(fread((dir()[2])))[rowsstart:rowsend,colstart:colend])
 
-system.time(list1<- tbl_df(as.data.frame(unlist(map1))))#2 minutes
-
-initial<- tbl_df(data.table( Class1 = rep(1/3, nrow(list1)),Class2 = rep(1/3, nrow(list1)),Class3 = rep(1/3, nrow(list1))))
+list1<- tbl_df(as.data.frame(unlist(map1)))#2 minutes
 
 list2<- tbl_df(as.data.frame(unlist(map2)))
 
+
+initial<- tbl_df(data.table( Class1 = rep(1/3, nrow(list1)),Class2 = rep(1/3, nrow(list1)),Class3 = rep(1/3, nrow(list1))))
+
+
 #list2<-list2)
 
-names(list1)[1]<- "Classes"
-names(list2)[1]<- "Classes"
-landscape.class<- na.omit(unique(list1))
-
-
+#names(list1)[1]<- "Classes"
+#names(list2)[1]<- "Classes"
+#landscape.class<- na.omit(unique(list1))
 
 confusiontable<- table(list2[[1]],list1[[1]])
 
@@ -58,19 +57,12 @@ prob.mat<-tbl_df(as.data.frame(prob.mat))
 
 new.mat<- prob.mat*initial
 
-post.prior<-t(apply(new.mat,1,function(x) x/sum(x)))
+posterior<-t(apply(new.mat,1,function(x) x/sum(x)))
 
-initial[list1[[1]] == list2[[1]],] <- post.prior[list1[[1]] == list2[[1]],]
+initial[list1[[1]] == list2[[1]],] <- posterior[list1[[1]] == list2[[1]],]
  
 new.list<- max.col(initial)
 
-#probability<- mutate((truthtable*0.925)+0.025)
-#test <- truthtable[1:100,]
-
-
-	
-
-
-#model <- naiveBayes(zz ~., data = 'aa')
+new.map<- matrix(new.list,dim(map1)[1],dim(map1[2]))
 
 
